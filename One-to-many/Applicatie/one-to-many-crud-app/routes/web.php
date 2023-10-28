40a08bb81356a69cbdb283a98dc1d811936ff7ee
PixelCrypth <RyanEHV@live.nl>
2023-10-28 18:37:13 +0200

    Authors aangemaakt voor one-to-many + instructie erbij

--- One-to-many/Applicatie/one-to-many-crud-app/routes/web.php
+++ One-to-many/Applicatie/one-to-many-crud-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('frontpage');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +30,26 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Method 1:
+Route::resources([
+    'authors' => AuthorController::class,
+    'books' => BookController::class,
+]);
+
+// Method 2:
+// Route::get('/books', [BookController::class, 'index'])->name('books.index');
+// Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+// Route::post('/books', [BookController::class, 'store'])->name('books.store');
+// Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+// Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+// Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+// Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+// Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
+// Route::get('/authors/create', [AuthorController::class, 'create'])->name('authors.create');
+// Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store');
+// Route::get('/authors/{author}', [AuthorController::class, 'show'])->name('authors.show');
+// Route::get('/authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit');
+// Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('authors.update');
+// Route::delete('/authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy');
 require __DIR__.'/auth.php';
